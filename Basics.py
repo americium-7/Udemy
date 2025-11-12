069b6e2568317155443e04c91e324694d6567499
americium-7 <adityamhetre009@gmail.com>
2025-11-12 18:59:31 +0530

    Update Basics.py

--- Basics.py
+++ Basics.py
@@ -1,1 +1,39 @@
+# this is the new start of python
+'''
+ This command is used for 
+many more 
+lines code
+'''
 
+print("Above commands are great")
+
+#Lets try addition 
+a=5
+b=3
+c=a+b
+print(c)
+
+#the output of above print is 8 
+
+'''now lets check with ("c")'''
+
+a=5
+b=3
+c=a+b
+print("The addition of a + b is:",c)
+
+#if as we see the output of this is only c.
+
+
+
+#if we want to go on next line we can use /n funtion
+#lets see
+
+a = ("Enter the age of aditya\n " )
+print(a)
+# user input 
+a = int(input ("Enter sum value \n"))
+print(a)
+
+if a%2==0:
+    print("The given number is Even:3",a)
