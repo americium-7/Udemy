e33f19847abbc16c8a1fcaca75144b1c4f4d8eb6
americium-7 <adityamhetre009@gmail.com>
2025-11-13 20:25:33 +0530

    Update screenshot.py

--- screenshot.py
+++ screenshot.py
@@ -1,18 +1,6 @@
 import time
 import pyautogui
-
-def screenshot():
-    time.sleep(2)
-    img = pyautogui.screenshot('test.png')
-    img.show()
-
-
-screenshot()
-# I changed this code with the below because in upper case code
-# The file name is always show the test 
-#due to that i am going to fix the changes with through 
-import time
-import pyautogui
+import tkinter as tk
 
 def screenshot():
     name = int(round(time.time() * 1000))
@@ -20,5 +8,25 @@ def screenshot():
     time.sleep(2)
     img = pyautogui.screenshot('test.png')
     img.show()
+
+root = tk.Tk()
+frame = tk.Frame(root)
+frame.pack()
+
+Button =tk.Button(
+    frame,
+    text="Take screenshot",
+    command=screenshot)
+
+Button.pack(side=tk.LEFT)
+close = tk.Button(
+    frame,
+    text="QUIT",
+command=quit)
+close.pack(side=tk.LEFT)
+
+root.mainloop()
+
+
 
 screenshot()
