46e7a0485bf79d4685a4ffa70c3c36904538eb28
americium-7 <adityamhetre009@gmail.com>
2025-11-13 20:03:49 +0530

    Update screenshot.py

--- screenshot.py
+++ screenshot.py
@@ -6,4 +6,19 @@ def screenshot():
     img = pyautogui.screenshot('test.png')
     img.show()
 
+
+screenshot()
+# I changed this code with the below because in upper case code
+# The file name is always show the test 
+#due to that i am going to fix the changes with through 
+import time
+import pyautogui
+
+def screenshot():
+    name = int(round(time.time() * 1000))
+    name = '{}. png'.format(name)
+    time.sleep(2)
+    img = pyautogui.screenshot('test.png')
+    img.show()
+
 screenshot()
