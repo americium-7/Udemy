1a155f58d8638259337e7744e49dbabf8b45d790
americium-7 <adityamhetre009@gmail.com>
2026-04-19 13:45:05 +0530

    Add files via upload

---
+++ face_detect.py
@@ -0,0 +1,30 @@
+import cv2
+
+def detect():
+    cap = cv2.VideoCapture(0)
+
+    face_cascade = cv2.CascadeClassifier(
+        cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
+    )
+
+    while True:
+        ret, img = cap.read()
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)
+
+        cv2.imshow('Face Detection', img)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+detect()
+
+
+'''Press Q to stop the code'''
