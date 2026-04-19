acafccbfee186f3bb7ce5b4602433ca352b22b3d
americium-7 <adityamhetre009@gmail.com>
2026-04-19 14:38:46 +0530

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+search = 'weather in solapur'
+url = f"https://www.google.com/search?&q={search}"
+
+r = requests.get(url)
+
+s = BeautifulSoup(r.text, "html.parser")
+
+update = s.find("div", class_="BNEawe").text
+print(update)
